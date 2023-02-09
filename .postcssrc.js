03c5ea00bd9fd1ad843f3f98e5d7f9d288b4e1ca
Junh0210 <wnsgus0210z@gmail.com>
2023-02-09 15:09:53 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,15 @@
+// ESM
+// CommonJS
+
+// import autoperficer from 'autoprefixer'
+
+// exprot {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }  
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
